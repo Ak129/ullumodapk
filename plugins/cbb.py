d90cad60db2f26a66032ba6780eecaeab9d98302
Ak129 <64674981+Ak129@users.noreply.github.com>
2023-10-11 00:11:23 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,9 +8,9 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "📜 About":
+    if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Contact Us : <a href='https://telegram.me/APKTHUGS_OWNER'>Click here</a>\n○ Channel : @ApkThugs\n○ Support Group : @ApkThugs_Chat</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Ak129/FileShare/upload'>Click here</a>\n○ Channel : @ApkThugs\n○ Support Group : @ApkThugs_Chat</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
